f2334440673d494126fc0e487065012b16041845
barbozacecilia <barboza.ceciliam@gmail.com>
2022-06-23 20:18:48 -0300

    feat(Environment): Add api endpoint to the environment

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,15 @@ export class ProductsService {
     private httpClient: HttpClient
   ) {}
   getAllProducts(){
-    return this.httpClient.get("https://api.mercadolibre.com/sites/MLA/search?q=iphone").pipe(map((value:any)=>value["results"]))
+    return this.httpClient.get(environment.apiEndPointMLA+"/sites/MLA/search?q=iphone").pipe(map((value:any)=>value["results"]))
   }
   getAllProductsProm(){
-    return lastValueFrom(this.httpClient.get("https://api.mercadolibre.com/sites/MLA/search?q=telescopio").pipe(map((value:any)=>value["results"])))
+    return lastValueFrom(this.httpClient.get(environment.apiEndPointMLA+"sites/MLA/search?q=telescopio").pipe(map((value:any)=>value["results"])))
   }
   getPicture(){
-    return lastValueFrom(this.httpClient.get("https://api.nasa.gov/planetary/apod?api_key=8cxzRvH8CXbIOfw9yT89gzz2fVvxswtdbA6nChUf"))
+    return lastValueFrom(this.httpClient.get(environment.apiEndPointApod+"?api_key=8cxzRvH8CXbIOfw9yT89gzz2fVvxswtdbA6nChUf"))
   }
   getById(id:any){
-    return lastValueFrom(this.httpClient.get(("https://api.mercadolibre.com/items/")+id))
+    return lastValueFrom(this.httpClient.get((environment.apiEndPointMLA+"/items/")+id))
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: false,
-  apiEndPoint:"ttps://api.mercadolibre.com/",
-  /*apiEndPointSecond:"https://api.nasa.gov/planetary/apod"*/
 };
